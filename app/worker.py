edfabde7e80549182a3139b904b52b5157b35af4
Mehedee Siddique <mehedees@live.com>
2023-08-25 09:21:13 +0000

    Update app with simplified api and task

--- app/worker.py
+++ app/worker.py
@@ -1,44 +1,23 @@
-import os
 from celery import Celery
-from celery.signals import worker_process_shutdown, worker_ready
-
-from .db_client import connect_to_db_sync, shutdown_db_sync, db
 
 
-# def find_tasks() -> list[str]:
-#     curr_dir =
-
-
-@worker_ready.connect
-def db_conn_init(*args, **kwargs):
-    """ Initialize a clean DB connection. """
-    if db.cluster is not None:
-        shutdown_db_sync()
-    connect_to_db_sync()
-
-
-@worker_process_shutdown.connect
-def cassandra_shutdown(*args, **kwargs):
-    """Close DB connection"""
-    shutdown_db_sync()
-
-
+# Broker(RabbitMQ) connection str
 CELERY_BROKER: str = (
     f"pyamqp://user:Pass1234@rabbitmq:5672//"
 )
+
+# Result Backend(Redis) connection str
 CELERY_BACKEND: str = f"redis://redis:6379"
+
+
+# Celery App instance
 celery_app = Celery(
     __name__, broker=CELERY_BROKER, backend=CELERY_BACKEND
 )
 
 
-celery_app.conf.broker_pool_limit = 500
-celery_app.conf.worker_prefetch_multiplier = 5
 celery_app.conf.worker_send_task_events = False
 celery_app.conf.task_send_sent_event = False
-celery_app.conf.task_ignore_result = True
-celery_app.conf.task_store_errors_even_if_ignored = True
-celery_app.conf.result_expires = 30
 
-
+# Autodiscovery of defined tasks
 celery_app.autodiscover_tasks(packages=['app'])
