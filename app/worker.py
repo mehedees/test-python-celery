820a6e1c8353c98312e5bfcc26936750db910295
mehedee <mehedees@live.com>
2023-09-19 01:43:02 +0600

    feat: Add periodic tasks entries to celery entries and set timezone

--- app/worker.py
+++ app/worker.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from celery.schedules import crontab
 
 
 # Broker(RabbitMQ) connection str
@@ -16,8 +17,19 @@ celery_app = Celery(
 )
 
 
-celery_app.conf.worker_send_task_events = False
-celery_app.conf.task_send_sent_event = False
-
 # Autodiscovery of defined tasks
 celery_app.autodiscover_tasks(packages=['app'])
+
+celery_app.conf.timezone = 'Asia/Dhaka'
+celery_app.conf.beat_schedule = {
+    'email_sending_task_0930_hour': {  # this key name doesn't mean anything
+        'task': 'tasks.send_weekly_newsletter_email_task',
+        'schedule': crontab(hour=9, minute=30, day_of_week=1),
+        'args': ()
+    },
+    'news_collecting_task_every_24_hours': {
+        'task': 'tasks.collect_daily_news_for_newsletter_task',
+        'schedule': 86400,  # every 24*60*60 seconds,
+        'args': ()
+    }
+}
